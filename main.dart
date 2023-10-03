bff9e64a6481f530c1c46c819517bf0d50585882
Yahir Zapata <zs21021768@estudiantes.uv.mx>
2023-10-02 23:00:03 -0600

    TiposDeDato

--- main.dart
+++ main.dart
@@ -16,4 +16,12 @@ void main(){
   //Dynamic *cambia de dato y puede ser redefinido
   dynamic numero = 2;
   numero = "2";
+
+  //
+  int edad1 = 12;
+  int edad2 = edad1++; //Primero asignacion luego incremento
+  edad2 = ++edad1; //Primero incremento luego asignacion
+
+  print(edad1);
+  print(edad2);
 }
