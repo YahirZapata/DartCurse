6072b25679ba7b1e6b54f264d6b253fca6f31661
Yahir Zapata <zs21021768@estudiantes.uv.mx>
2023-10-02 22:56:35 -0600

    TiposDeDato

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main(){
+  //Tipos de datos
+
+  int edad = 20;
+  double estatura = 1.92;
+  bool verdadero = true;
+  String nombre = "Yahir";
+  List<String> nombres = ["Julio", "Gerly", "Manzano"];
+
+
+
+  //Const y Final
+  const Nombre = "Yahir";
+  print(nombre);
+
+  //Dynamic *cambia de dato y puede ser redefinido
+  dynamic numero = 2;
+  numero = "2";
+}
